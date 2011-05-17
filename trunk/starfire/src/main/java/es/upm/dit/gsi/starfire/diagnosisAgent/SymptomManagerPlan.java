48f65d5c40f33b23dea8fdccd254a5f84edce4be
jlgconde07@gmail.com <jlgconde07@gmail.com@dcce0ac7-a09d-9135-e996-7cf71fbf759c>
2011-05-17 11:17:27 +0000

    Rename packages to starfire

---
+++ trunk/starfire/src/main/java/es/upm/dit/gsi/starfire/diagnosisAgent/SymptomManagerPlan.java
@@ -0,0 +1,75 @@
+package es.upm.dit.gsi.starfire.diagnosisAgent;
+
+import edu.stanford.smi.protegex.owl.model.OWLModel;
+import communeOntology.Diagnosis;
+import communeOntology.MyFactory;
+import communeOntology.Symptom;
+import jadex.bdi.runtime.Plan;
+
+public class SymptomManagerPlan extends Plan {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -8174299593349854500L;
+
+	@Override
+	public void body() {
+		// TODO Auto-generated method stub
+		
+		//Plan sensible a síntomas
+		//En el de sintomas creo un nuevo diagnostico
+		//Extraigo observaciones
+		
+		//Obtengo el síntoma
+		Symptom symptom = getSymptom();
+			
+		//Creo el diagnóstico comenzado por este síntoma
+		Diagnosis diagnosis = createDiagnosis(symptom);
+				
+		//Actualiza la ontología con el nuevo diagnóstico y el síntoma recibido
+		updateOntology(diagnosis,symptom);
+		
+		//Crear los individuos de todas las acciones
+		OWLModel owlModel = (OWLModel) getBeliefbase().getBelief("ontology").getFact();
+		MyFactory myFactory = new MyFactory(owlModel);		
+		
+		//TODO: Crear dinámicamente un individuo de cada tipo de acción
+		myFactory.createRTPMonitoringAction(null);
+		myFactory.createConnectivityTestAction(null);
+		myFactory.createNetworkInterfaceRateTest(null);
+		myFactory.createUsageCPUTest(null);
+		myFactory.createUsageMemoryTest(null);
+
+		//Lanza DiagnosisLoopPlan
+		throwDiagnosisLoopPlan();
+	}
+
+	private Symptom getSymptom() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	private Diagnosis createDiagnosis(Symptom symptom) {
+		// TODO Auto-generated method stub
+		
+		//Creo el diagnóstico comenzado por el síntoma
+			//Diagnosis diagnosis = new Diagnosis(...);
+		//Pongo la relación entre el diagnóstico y el síntoma de empezado por
+		
+		//poner identificador al diagnóstico con timestamp, nombre del agente, nombre de la máquina
+
+		
+		return null;
+	}
+	
+	private void updateOntology(Diagnosis diagnosis, Symptom symptom) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	private void throwDiagnosisLoopPlan() {
+		// TODO Auto-generated method stub
+		
+	}
+}
