a62992430aa0553c3eb44f368f98ccb23e9424ca
jeritous17@gmail.com <jeritous17@gmail.com@dcce0ac7-a09d-9135-e996-7cf71fbf759c>
2011-05-30 17:39:03 +0000

    Enter information about where are the files

--- branches/bnOWLTranslator/src/main/java/test/Definitions.java
+++ branches/bnOWLTranslator/src/main/java/test/Definitions.java
@@ -2,7 +2,9 @@ package test;
 
 public class Definitions {
 
-	public static final String uriNet ="";
-	public static final String uriOwlEmpty ="";
+	public static final String uriNet ="C:\\Documents and Settings\\Usuario\\Escritorio\\workspace\\bnOWLTranslator\\PruebaLLuviaEstados.net";
+	public static final String uriOwlEmpty ="C:\\Documents and Settings\\Usuario\\Escritorio\\workspace\\bnOWLTranslator\\Example\\owl\\EmptyBN.owl";
+	public static final String uriNetFinal = "C:\\Documents and Settings\\Usuario\\Escritorio\\workspace\\bnOWLTranslator\\NET\\PruebaLLuviaEstados.net";
+	public static final String uriOwl = "C:\\Documents and Settings\\Usuario\\Escritorio\\workspace\\bnOWLTranslator\\PruebaLLuviaEstados.owl";
 	
 }
