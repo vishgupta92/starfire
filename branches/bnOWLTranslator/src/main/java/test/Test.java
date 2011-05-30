962d5a91ed373e5c4f490cc9de5f6152bf8d4a68
jeritous17@gmail.com <jeritous17@gmail.com@dcce0ac7-a09d-9135-e996-7cf71fbf759c>
2011-05-30 17:40:25 +0000

    change test(net-owl-net) to net-net, and do multiple tests

--- branches/bnOWLTranslator/src/main/java/test/Test.java
+++ branches/bnOWLTranslator/src/main/java/test/Test.java
@@ -34,216 +34,358 @@ public class Test extends TestCase {
 	String uriNet;
 	String uriOwl;
 	String uriOwlEmpty;
-	String uriNetAfter;
-	String nameNet;
+	String uriNetFinal;
 
-	public Test(){
-		uriNet = "";
-		uriOwl = "";
-		uriOwlEmpty = "";
-		nameNet = "";
+	public void createFiles(){
+		uriNet = Definitions.uriNet;;
+		uriOwl = Definitions.uriOwl;
+		uriOwlEmpty = Definitions.uriOwlEmpty;
+		uriNetFinal = Definitions.uriNetFinal;
+		
 		bayesT = new BayesianTranslatorNet2OWL(uriNet, uriOwlEmpty);
-		bbT = new UnBBayesTranslatorOWL2Net(uriOwl, nameNet);
-
-		
-		String aux = Definitions.uriNet;
-		
+		bbT = new UnBBayesTranslatorOWL2Net(new File(uriOwl), uriNetFinal);		
 		//Generation of files
+		boolean actual = false;
+		try{
 		bayesT.convertNet2Owl();
 		bbT.convertOwlToNet();
+		actual = true;
+		}catch(Exception e){
+			System.out.println("Error: " + e);
+			actual = false;
+		}
+		
+		assertEquals(true, actual);
 
 	}
-	public void createNetAndOwl() {
-		//  NetBefore
+	
+	
+	public void loadNetBefore() {
 		netBefore = null;
-
+		boolean loaded = false;
 		try {
 			BaseIO io = new NetIO();
 			netBefore = (ProbabilisticNetwork) io.load(new File(uriNet));
+			loaded = true;
 
 		} catch (Exception e) {
 			e.printStackTrace();
 			System.exit(1);
+			loaded = false;
 		}
-		//owl
-		try {
-			FileReader owlFile = new FileReader(uriOwl);
-			owlModel = ProtegeOWL.createJenaOWLModelFromReader(owlFile);
-			owlModel.getNamespaceManager().setDefaultNamespace(
-			"http://www.gsi.upm.es/Commune.owl#");
+		
+		assertEquals(true, loaded);
 
-		} catch (Exception oe) {
-			oe.printStackTrace();
-
-		}
-		this.myFactory = new MyFactory(this.owlModel);
-
-		// NetAfter
+	}
+	public void loadNetAfter(){
+		
 		netAfter = null;
-
+		boolean loaded = false;
 		try {
 			BaseIO io = new NetIO();
-			netAfter = (ProbabilisticNetwork) io.load(new File(uriNetAfter));
+			netAfter = (ProbabilisticNetwork) io.load(new File(uriNetFinal));
+			loaded = true;
 
 		} catch (Exception e) {
 			e.printStackTrace();
 			System.exit(1);
+			loaded = false;
 		}
+		assertEquals(true, loaded);
+	}
+	
+	
+	public void checkNumNodesNet(){
+		
+		//net before
+		int numNodesB = netBefore.getNodes().size();
+		
+		//net after
+		int numNodesA = netAfter.getNodes().size();
+		
+		assertEquals(numNodesB, numNodesA);
 
 	}
-
-	/**
-	 * Check if the names of the nodes that 
-	 * are in the redBayesiana are the same that exist in the ontology
-	 */
-	public void checkNodesOwlNet() {
-		ArrayList<Node> nodesNet = netBefore.getNodes();
-		Set<Domain_Res> nodesOwl = myFactory.getAllDomain_ResInstances();
-
-		Set<String> nodesNetName = new HashSet<String>();
-		Set<String> nodesOwlName = new HashSet<String>();
-		for(Node node : nodesNet) {
-			String nameNode = node.getName();
-			nodesNetName.add(nameNode);
-		}
-		for(Domain_Res res : nodesOwl) {
-			String nameNode = res.getLocalName();
-			nodesOwlName.add(nameNode);
-		}
-
-		assertEquals(nodesOwlName, nodesNetName);
+	
+	public void checkNameNodesNet(){
+		//net before
+		int numNodesB = netBefore.getNodes().size();	
+		int random = new Double(Math.random() * numNodesB).intValue();	
+		System.out.println("numero nodos :"+ numNodesB +", numero random :" + random );
+		int numberOfNode = random - 1;
+		Node nodeB = netBefore.getNodes().get(numberOfNode);
+		
+		// net after
+		Node nodeA = netAfter.getNode(nodeB.getName());
+		
+		assertEquals(nodeA, nodeB);
 
 	}
-
-	/**
-	 * Checks the status of each
-	 *  node are the same in the Bayesian network and the ontology
-	 */
-	public void nodeStatesEqual() {
-		ArrayList<Node> nodesNet = netBefore.getNodes();
-		Set<Domain_Res> nodesOwl = myFactory.getAllDomain_ResInstances();
-		boolean equals = true;
-		for(Node node : nodesNet) {
-			Set<String> statesNodeNet = new HashSet<String>();
-			for(int i = 0; i<node.getStatesSize(); i++){
-				statesNodeNet.add(node.getStateAt(i));
-			}
-
-			for(Domain_Res res : nodesOwl) {
-				//a�adir el set con los estados que tiene uno
-				Set<String> statesNodeOwl = new HashSet<String>();
-				//`poner el nombre de cada estado en el set anterior
-
-				if(node.getName().equals(res.getLocalName())) {
-					if(!statesNodeOwl.equals(statesNodeNet)) {
-						equals = false;
-					}
-					break;
-				}
-
-
-			}
-
+	
+	public void checkStatesNodesNet(){
+		//net before
+		int numNodesB = netBefore.getNodes().size();	
+		int random = new Double(Math.random() * numNodesB).intValue();	
+		System.out.println("numero nodos :"+ numNodesB +", numero random :" + random );
+		int numberOfNode = random - 1;
+		Node nodeB = netBefore.getNodes().get(numberOfNode);
+		ArrayList<String> nodeStatesB = new ArrayList<String>();
+		for(int i = 0; i<nodeB.getStatesSize(); i++){
+			nodeStatesB.add(nodeB.getStateAt(i));
 		}
-		assertEquals(equals, true);
-
-	}
-
-	/**
-	 * Compare if each node has the same parents in Bayesian Network and Owl
-	 */
-	public void checkParentsNode() {
-		ArrayList<Node> nodesNet = netBefore.getNodes();
-		Set<Domain_Res> nodesOwl = myFactory.getAllDomain_ResInstances();
-		boolean expected = true;
-		boolean now = true;
-		for(Node node : nodesNet) {
-			ProbabilisticNode pNode = (ProbabilisticNode)node;
-			ArrayList<Node> parentsNodeNet = pNode.getParents(); 
-			Set<String> parentsNodeNetName = new HashSet<String>();
-			for(int i = 0; i<parentsNodeNet.size(); i++){
-				String nameParent = parentsNodeNet.get(i).getName();
-				parentsNodeNetName.add(nameParent);					
+		
+		boolean contain = true;
+		//net After
+		Node nodeA = netAfter.getNode(nodeB.getName());
+		for(int j = 0; j < nodeA.getStatesSize(); j++){
+			String stateA = nodeA.getStateAt(j);
+			
+			if(!nodeStatesB.contains(stateA)){
+				contain = false;
 			}
-
-			for(Domain_Res res : nodesOwl) {
-				//a�adir el set con los padres que tiene uno
-				Set<String> parentsNodeOwlName = new HashSet<String>();
-				//`poner el nombre de cada estado en el set anterior
-
-				for (String parentOwlName : parentsNodeOwlName) {
-					boolean found = false;
-					for (String parentNetName : parentsNodeNetName) {
-						if (parentOwlName.equals(parentOwlName)) {
-							found = true;
-						}
-					}
-					
-				}
-				
-				if(node.getName().equals(res.getLocalName())) {
-					if(!parentsNodeOwlName.equals(parentsNodeNetName)) {
-						now = false;
-					}
-					break;
-				}
-
-			}
-
 		}
-		assertEquals(expected, now);
-
-
+		
+		assertEquals(true, contain);		
 	}
-
-	/**
-	 * Compare marginal tables of each node and net owl
-	 */
-	public void compareTables() {
-		ArrayList<Node> nodesNet = netBefore.getNodes();
-		Set<Domain_Res> nodesOwl = myFactory.getAllDomain_ResInstances();
-		Set<PR_OWLTable> tablesOwl = myFactory.getAllPR_OWLTableInstances();
-		boolean expected = true;
-		boolean now = true;
-		for(Node node : nodesNet) {
-			ProbabilisticNode pNode = (ProbabilisticNode)node;
-			PotentialTable tableNode = pNode.getProbabilityFunction();
-			Set<Double> marginalsNodeNet = new HashSet<Double>(); 
-			for(int i= 0;i<tableNode.getVariablesSize();i++){
-				marginalsNodeNet.add((double) tableNode.getValue(i));	
+	
+	
+	public void checkParentsNodeNet(){
+		
+		//net before
+		int numNodesB = netBefore.getNodes().size();	
+		int random = new Double(Math.random() * numNodesB).intValue();	
+		System.out.println("numero nodos :"+ numNodesB +", numero random :" + random );
+		int numberOfNode = random - 1;
+		Node nodeB = netBefore.getNodes().get(numberOfNode);
+		ArrayList<Node> nodeParentsB = nodeB.getParents();
+		
+		
+		boolean contain = true;
+		//net After
+		Node nodeA = netAfter.getNode(nodeB.getName());
+		ArrayList<Node> nodeParentsA = nodeA.getParents();
+		for(int j = 0; j < nodeParentsA.size(); j++){
+			 Node parentA = nodeParentsA.get(j);
+			
+			if(!nodeParentsB.contains(parentA)){
+				contain = false;
 			}
-
-			for(Domain_Res res : nodesOwl){
-
-
-				if(node.getName().equals(res.getLocalName())) {
-					Set<Double> marginalsNodeOwl = new HashSet<Double>();
-
-					for(PR_OWLTable table : tablesOwl) {
-						Domain_Res resTable = (Domain_Res) table.getIsProbDistOf();
-						if(resTable.equals(res)) {
-							//cogemos cada una de las assign y cogemos su valor
-							table.getHasProbAssign();
-							//las a�adimos al marginalsNodeOwl
-//							if(!parentsNodeOwlName.equals(parentsNodeNetName)) {
-//								now = false;
-//							}
-
-							break;
-						}
-					}
-
-
-				}
-
-			}
-
 		}
-
-
-
-		assertEquals(expected, now);
+		
+		assertEquals(true, contain);		
 	}
+		
+	
+	
+public void checkChildrenNodeNet(){
+	
+	//net before
+	int numNodesB = netBefore.getNodes().size();	
+	int random = new Double(Math.random() * numNodesB).intValue();	
+	System.out.println("numero nodos :"+ numNodesB +", numero random :" + random );
+	int numberOfNode = random - 1;
+	Node nodeB = netBefore.getNodes().get(numberOfNode);
+	ArrayList<Node> nodeChildrenB = nodeB.getChildren();
+	
+	
+	boolean contain = true;
+	//net After
+	
+	Node nodeA = netAfter.getNode(nodeB.getName());
+	ArrayList<Node> nodeChildrenA = nodeA.getChildren();
+	for(int j = 0; j < nodeChildrenA.size(); j++){
+		 Node childA = nodeChildrenA.get(j);
+		
+		if(!nodeChildrenB.contains(childA)){
+			contain = false;
+		}
+	}
+	
+	assertEquals(true, contain);		
+}
+	
+
+
+public void checkPotentialTableNodeNet(){
+	//net before
+	int numNodesB = netBefore.getNodes().size();	
+	int random = new Double(Math.random() * numNodesB).intValue();	
+	System.out.println("numero nodos :"+ numNodesB +", numero random :" + random );
+	int numberOfNode = random - 1;
+	Node nodeB = netBefore.getNodes().get(numberOfNode);
+	ProbabilisticNode pNodeB = (ProbabilisticNode) nodeB;
+	PotentialTable tableB = pNodeB.getProbabilityFunction();
+	double valueB = tableB.getValue(0);//mirar esta parte.
+	
+	//net After 
+	
+	Node nodeA = netAfter.getNode(nodeB.getName());
+	ProbabilisticNode pNodeA = (ProbabilisticNode) nodeA;
+	PotentialTable tableA = pNodeA.getProbabilityFunction();
+	double valueA = tableA.getValue(0); //mirar esta parte
+	
+	
+	assertEquals(valueB, valueA);
+	
+
+	
+}
+	
+	
+	
+//
+//	/**
+//	 * Check if the names of the nodes that 
+//	 * are in the redBayesiana are the same that exist in the ontology
+//	 */
+//	public void checkNodesOwlNet() {
+//		ArrayList<Node> nodesNet = netBefore.getNodes();
+//		Set<Domain_Res> nodesOwl = myFactory.getAllDomain_ResInstances();
+//
+//		Set<String> nodesNetName = new HashSet<String>();
+//		Set<String> nodesOwlName = new HashSet<String>();
+//		for(Node node : nodesNet) {
+//			String nameNode = node.getName();
+//			nodesNetName.add(nameNode);
+//		}
+//		for(Domain_Res res : nodesOwl) {
+//			String nameNode = res.getLocalName();
+//			nodesOwlName.add(nameNode);
+//		}
+//
+//		assertEquals(nodesOwlName, nodesNetName);
+//
+//	}
+//
+//	/**
+//	 * Checks the status of each
+//	 *  node are the same in the Bayesian network and the ontology
+//	 */
+//	public void nodeStatesEqual() {
+//		ArrayList<Node> nodesNet = netBefore.getNodes();
+//		Set<Domain_Res> nodesOwl = myFactory.getAllDomain_ResInstances();
+//		boolean equals = true;
+//		for(Node node : nodesNet) {
+//			Set<String> statesNodeNet = new HashSet<String>();
+//			for(int i = 0; i<node.getStatesSize(); i++){
+//				statesNodeNet.add(node.getStateAt(i));
+//			}
+//
+//			for(Domain_Res res : nodesOwl) {
+//				//a�adir el set con los estados que tiene uno
+//				Set<String> statesNodeOwl = new HashSet<String>();
+//				//`poner el nombre de cada estado en el set anterior
+//
+//				if(node.getName().equals(res.getLocalName())) {
+//					if(!statesNodeOwl.equals(statesNodeNet)) {
+//						equals = false;
+//					}
+//					break;
+//				}
+//
+//
+//			}
+//
+//		}
+//		assertEquals(equals, true);
+//
+//	}
+//
+//	/**
+//	 * Compare if each node has the same parents in Bayesian Network and Owl
+//	 */
+//	public void checkParentsNode() {
+//		ArrayList<Node> nodesNet = netBefore.getNodes();
+//		Set<Domain_Res> nodesOwl = myFactory.getAllDomain_ResInstances();
+//		boolean expected = true;
+//		boolean now = true;
+//		for(Node node : nodesNet) {
+//			ProbabilisticNode pNode = (ProbabilisticNode)node;
+//			ArrayList<Node> parentsNodeNet = pNode.getParents(); 
+//			Set<String> parentsNodeNetName = new HashSet<String>();
+//			for(int i = 0; i<parentsNodeNet.size(); i++){
+//				String nameParent = parentsNodeNet.get(i).getName();
+//				parentsNodeNetName.add(nameParent);					
+//			}
+//
+//			for(Domain_Res res : nodesOwl) {
+//				//a�adir el set con los padres que tiene uno
+//				Set<String> parentsNodeOwlName = new HashSet<String>();
+//				//`poner el nombre de cada estado en el set anterior
+//
+//				for (String parentOwlName : parentsNodeOwlName) {
+//					boolean found = false;
+//					for (String parentNetName : parentsNodeNetName) {
+//						if (parentOwlName.equals(parentOwlName)) {
+//							found = true;
+//						}
+//					}
+//					
+//				}
+//				
+//				if(node.getName().equals(res.getLocalName())) {
+//					if(!parentsNodeOwlName.equals(parentsNodeNetName)) {
+//						now = false;
+//					}
+//					break;
+//				}
+//
+//			}
+//
+//		}
+//		assertEquals(expected, now);
+//
+//
+//	}
+//
+//	/**
+//	 * Compare marginal tables of each node and net owl
+//	 */
+//	public void compareTables() {
+//		ArrayList<Node> nodesNet = netBefore.getNodes();
+//		Set<Domain_Res> nodesOwl = myFactory.getAllDomain_ResInstances();
+//		Set<PR_OWLTable> tablesOwl = myFactory.getAllPR_OWLTableInstances();
+//		boolean expected = true;
+//		boolean now = true;
+//		for(Node node : nodesNet) {
+//			ProbabilisticNode pNode = (ProbabilisticNode)node;
+//			PotentialTable tableNode = pNode.getProbabilityFunction();
+//			Set<Double> marginalsNodeNet = new HashSet<Double>(); 
+//			for(int i= 0;i<tableNode.getVariablesSize();i++){
+//				marginalsNodeNet.add((double) tableNode.getValue(i));	
+//			}
+//
+//			for(Domain_Res res : nodesOwl){
+//
+//
+//				if(node.getName().equals(res.getLocalName())) {
+//					Set<Double> marginalsNodeOwl = new HashSet<Double>();
+//
+//					for(PR_OWLTable table : tablesOwl) {
+//						Domain_Res resTable = (Domain_Res) table.getIsProbDistOf();
+//						if(resTable.equals(res)) {
+//							//cogemos cada una de las assign y cogemos su valor
+//							table.getHasProbAssign();
+//							//las a�adimos al marginalsNodeOwl
+////							if(!parentsNodeOwlName.equals(parentsNodeNetName)) {
+////								now = false;
+////							}
+//
+//							break;
+//						}
+//					}
+//
+//
+//				}
+//
+//			}
+//
+//		}
+//
+//
+//
+//		assertEquals(expected, now);
+//	}
 
 
 
