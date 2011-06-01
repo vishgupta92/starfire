9d530f89994b9a5744631c373c274dab6ea86d19
jeritous17@gmail.com <jeritous17@gmail.com@dcce0ac7-a09d-9135-e996-7cf71fbf759c>
2011-06-01 15:17:45 +0000

    UnBBayes Commit

---
+++ src/main/java/unbbayes/prs/builder/impl/DefaultContinuousNodeBuilder.java
@@ -0,0 +1,53 @@
+/**
+ * 
+ */
+package unbbayes.prs.builder.impl;
+
+import unbbayes.prs.Node;
+import unbbayes.prs.builder.INodeBuilder;
+import unbbayes.prs.builder.IProbabilisticNetworkBuilder;
+import unbbayes.prs.hybridbn.ContinuousNode;
+
+/**
+ * @author Shou Matsumoto
+ *
+ */
+public class DefaultContinuousNodeBuilder implements INodeBuilder {
+
+	/**
+	 * Default implementation of builder for continuous nodes
+	 * 
+	 * @see IProbabilisticNetworkBuilder
+	 * @see INodeBuilder
+	 */
+	protected DefaultContinuousNodeBuilder() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	/**
+	 * Construction method for 
+	 * default implementation of builder for continuous nodes
+	 * 
+	 * @see IProbabilisticNetworkBuilder
+	 * @see INodeBuilder
+	 */
+	public static DefaultContinuousNodeBuilder newInstance(){
+		return new DefaultContinuousNodeBuilder();
+	}
+
+	/* (non-Javadoc)
+	 * @see unbbayes.io.builder.INodeBuilder#buildNode()
+	 */
+	public Node buildNode() {
+		return new ContinuousNode();
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see unbbayes.prs.builder.INodeBuilder#getNodeClass()
+	 */
+	public Class getNodeClass() {
+		return ContinuousNode.class;
+	}
+
+}
